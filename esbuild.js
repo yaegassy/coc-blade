578a68a5889457fba14de368b1fb88c44a60f5d5
yaegassy <yaegassy@users.noreply.github.com>
2022-11-21 10:14:10 +0900

    chore: change esbuild target to node14.14
    update as well as coc.nvim.

--- esbuild.js
+++ esbuild.js
@@ -9,7 +9,7 @@ async function start(watch) {
     mainFields: ['module', 'main'],
     external: ['coc.nvim', 'vscode-oniguruma/release/onig.wasm'],
     platform: 'node',
-    target: 'node10.12',
+    target: 'node14.14',
     outfile: 'lib/index.js',
   });
 }

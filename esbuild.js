c1b8af53393532ccb2d8f142668295c98f2e9c32
yaegassy <yaegassy@users.noreply.github.com>
2023-04-20 12:31:28 +0900

    refactor: revert esbuild.js

--- esbuild.js
+++ esbuild.js
@@ -1,20 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 async function start(watch) {
-  const plugins = [
-    {
-      name: 'watch-mode-plugin',
-      setup(build) {
-        let count = 0;
-        build.onEnd((result) => {
-          if (count++ === 0) console.log('first build:', result);
-          else console.log('subsequent build:', result);
-        });
-      },
-    },
-  ];
-
-  const buildOptions = {
+  await require('esbuild').build({
     entryPoints: ['src/index.ts'],
     bundle: true,
+    watch,
     minify: process.env.NODE_ENV === 'production',
     sourcemap: process.env.NODE_ENV === 'development',
     mainFields: ['module', 'main'],
@@ -22,21 +11,23 @@ async function start(watch) {
     platform: 'node',
     target: 'node14.14',
     outfile: 'lib/index.js',
-  };
-
-  if (watch) {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const ctx = await require('esbuild').context({ ...buildOptions, plugins });
-    await ctx.watch();
-  } else {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const ctx = await require('esbuild').context({ ...buildOptions });
-    await ctx.rebuild();
-    ctx.dispose();
-  }
+    logLevel: 'error',
+  });
 }
 
-const watch = process.argv.length > 2 && process.argv[2] === '--watch' ? true : false;
+let watch = false;
+if (process.argv.length > 2 && process.argv[2] === '--watch') {
+  console.log('watching...');
+  watch = {
+    onRebuild(error) {
+      if (error) {
+        console.error('watch build failed:', error);
+      } else {
+        console.log('watch build succeeded');
+      }
+    },
+  };
+}
 
 start(watch).catch((e) => {
   console.error(e);
